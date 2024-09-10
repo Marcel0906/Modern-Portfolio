fb70bc6b7e3cee43446d77ee485ca1fd8eb755cd
Marcel0906 <m.kubetin89@gmail.com>
2024-09-10 16:03:04 +0200

    First Step with Packages

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Modern-Portfolio
+
+## created with Next.js, TailwindCSS, TypeScript and shadcn/ui components
---
+++ portfolio/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+    <section className='py-24'>
+    <div className='container'>
+      <h1 className='text-4xl font-bold text-center'>Welcome to the Portfolio</h1>
+    </div>
+    </section>
+   
+  )
+}
