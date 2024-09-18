9a48d12938f5c3ac3bc3069300624d0f0ca8a879
Marcel0906 <m.kubetin89@gmail.com>
2024-09-18 11:04:09 +0200

    svg size update

--- portfolio/components/about.tsx
+++ portfolio/components/about.tsx
@@ -53,72 +53,72 @@ export default function About() {
               src='/images/svg/HTML5.svg'
               alt='html5'
               title='HTML5 | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/CSS3.svg'
               alt='css3'
               title='CSS3 | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/JS.svg'
               alt='javascript'
               title='JavaScript | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/NodeJS.svg'
               alt='node.js'
               title='NodeJS | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/TS.svg'
               alt='typescript'
               title='TypeScript | gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/cra-logo.svg'
               alt='create-react-app'
               title='Create-React-App | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/React.svg'
               alt='react'
               title='React | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/ViteJS.svg'
               alt='vite.js'
               title='Vite.js | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
                     <Image
               src='/images/svg/nextjs.svg'
               alt='next.js'
               title='Next.js | gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
 
@@ -126,136 +126,136 @@ export default function About() {
               src='/images/svg/SASS.svg'
               alt='sass'
               title='SASS | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/expressJS.svg'
               alt='express.js'
               title='Express | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/MongoDB.svg'
               alt='mongodb'
               title='MongoDB | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/mongoose-original.svg'
               alt='mongoose'
               title='Mongoose | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/MySQL.svg'
               alt='mysql'
               title='MySQL | erweiterte Grundkenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
               <Image
               src='/images/svg/GraphQL.svg'
               alt='graphql'
               title='GraphQL | Grundkenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/PHP8.svg'
               alt='php8'
               title='PHP8 | erweiterte Grundkenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/JSON.svg'
               alt='json'
               title='JSON | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/jwt.svg'
               alt='jwt'
               title='JSON Web Token | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/MarkDown.svg'
               alt='markdown'
               title='MarkDown | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/BootStrap.svg'
               alt='bootstrap'
               title='BootStrap | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/React-BootStrap.svg'
               alt='react-bootstrap'
               title='React-BootStrap | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/React-Router.svg'
               alt='react-router'
               title='React-Router | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/React-PostCSS.svg'
               alt='react-postcss'
               title='React-PostCSS | gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/react-icons.svg'
               alt='react-icons'
               title='React-Icons | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/TailwindCSS.svg'
               alt='tailwindcss'
               title='TailwindCSS | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/npm.svg'
               alt='npm'
               title='NPM | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
           </div>
@@ -265,64 +265,64 @@ export default function About() {
               src='/images/svg/Git.svg'
               alt='git'
               title='Git | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/GitHub.svg'
               alt='github'
               title='GitHub | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/Github-desktop-logo-symbol.svg'
               alt='github-desktop'
               title='GitHub-Desktop | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/Vercel.svg'
               alt='vercel'
               title='Vercel | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/MS-VSCode.svg'
               alt='vscode'
               title='MS VSCode | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/Postman.svg'
               alt='postman'
               title='Postman | sehr gute Kenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
                   <Image
               src='/images/svg/WordPress.svg'
               alt='wordpress'
               title='WordPress | gute Grundkenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/Typo3.svg'
               alt='typo3'
               title='Typo3 | sehr gute Grundkenntnisse'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
           </div>
@@ -332,64 +332,64 @@ export default function About() {
               src='/images/svg/Trello.svg'
               alt='trello'
               title='Trello'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/Slack.svg'
               alt='slack'
               title='Slack'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/zoom-call.svg'
               alt='zoom'
               title='Zoom'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/Microsoft_Office_Teams.svg'
               alt='microsoft-teams'
               title='Microsoft Teams'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/LinkedIn.svg'
               alt='linkedin'
               title='LinkedIn'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/GIMP.svg'
               alt='gimp'
               title='GIMP'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/Figma.svg'
               alt='figma'
               title='Figma'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/Discord.svg'
               alt='discord'
               title='Discord'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
           </div>
@@ -399,16 +399,16 @@ export default function About() {
               src='/images/svg/ChatGPT.svg'
               alt='chatgpt'
               title='ChatGPT'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             <Image
               src='/images/svg/Github-Copilot.svg'
               alt='github-copilot'
               title='Github-Copilot'
-              width={1000}
-              height={1000}
+              width={200}
+              height={200}
               className='rounded-lg'
             />
             </div>
