cdad68a9cf7f56e8576ab3636d236e56238c306a
Marcel0906 <m.kubetin89@gmail.com>
2024-09-19 10:24:13 +0200

    intro update

--- portfolio/components/intro.tsx
+++ portfolio/components/intro.tsx
@@ -7,7 +7,7 @@ export default function Intro() {
       <div className='mt-2 flex-1 md:mt-0'>
         <h1 className='title no-underline'>Hi, ich bin Marcel Kubetin.</h1>
         <p className='mt-3 font-light text-muted-foreground'>
-          Als Full-Stack Web Developer aus Bielefeld, Deutschland, entwickele ich mit
+          Als Full-Stack Web Developer aus Bielefeld, Deutschland, entwickle ich mit
           Leidenschaft Webanwendungen. In einem dynamischen Team kann ich mein Wissen und meine Fähigkeiten 
           einbringen und innovative Weblösungen entwickeln. Auf der Suche nach neuen Herausforderungen und Möglichkeiten, hoffe ich auf spannende Projekte.
         </p>
